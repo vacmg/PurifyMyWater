4c3f2030e6d6110151c0ed00f26ef2abc4a21f92
vacmg <70143287+vacmg@users.noreply.github.com>
2020-08-27 22:00:06 +0200

    Add files via upload

---
+++ Tests/Serial_Test/Handshake/MEGA/MEGA.ino
@@ -0,0 +1,41 @@
+const unsigned long timeout = 5000; // 5 seconds
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+Serial.println(F("HELLO"));
+Serial1.begin(9600);
+handshake();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void handshake()
+{
+  unsigned long pmillis = millis();
+  while (!Serial1.available() && (millis() < (pmillis + timeout)))
+  {
+    Serial1.print('A');
+    delay(300);
+  }
+  bool ok = 0;
+  while(Serial1.available())
+  {
+    if (Serial1.read() == 'Z')
+    {
+      ok = 1;
+    }
+  }
+  if (ok)
+  {
+    Serial.println(F("SUCCESSFUL HANDSHAKE"));
+  }
+  else
+  {
+    Serial.println(F("HANDSHAKE ERROR"));
+    while(1);
+  }
+}
