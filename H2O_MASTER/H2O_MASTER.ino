69404563863476e12c924a3fd0e298d65788e551
vacmg <vacmg1@gmail.com>
2020-12-20 14:10:25 +0100

    Fix some relay bugs

--- H2O_MASTER/H2O_MASTER.ino
+++ H2O_MASTER/H2O_MASTER.ino
@@ -42,6 +42,15 @@ const byte tempPin = 48;
     /*------------Input-----------------*/
 
     /*------------Output----------------*/
+#define BLACK 0
+#define RED 1
+#define GREEN 2
+#define BLUE 3
+#define WHITE 4
+#define YELLOW 5
+#define CYAN 6
+#define PURPLE 7
+
 const byte voltSSRelay = 33;
 const byte voltRelay = 49;
 const byte ACInverter = 45;
@@ -89,13 +98,70 @@ float temp[3];
 
 // Depending on the hardware used, relays are activated with high or low signals.
 // To maintain readability, in this function we set what kind of signal we need to activate those relays
+// A true value means that the circuit is closed whereas a false means that the circuit is opened
 void output(byte pin, bool value)
 {
-    if (!(pin == redLed || pin == greenLed || pin == blueLed))
+    if (!(pin == redLed || pin == greenLed || pin == blueLed || pin == voltRelay || pin == outFan || pin == PSUFan || pin == inFan))
     {
         value = !value;
     }
     digitalWrite(pin, value);
+}
+
+// Used to set a color on the rgb status led by setting the amount of each color from 0 to 255
+void setColor(byte r, byte g, byte b)
+{
+    analogWrite(redLed, r);
+    analogWrite(greenLed, g);
+    analogWrite(blueLed, b);
+}
+
+// Used to set a color on the rgb status led using a predefined color
+void setColor(byte color)
+{
+    switch (color)
+    {
+    case RED:
+        output(redLed, 1);
+        output(greenLed, 0);
+        output(blueLed, 0);
+        break;
+    case GREEN:
+        output(redLed, 0);
+        output(greenLed, 1);
+        output(blueLed, 0);
+        break;
+    case BLUE:
+        output(redLed, 0);
+        output(greenLed, 0);
+        output(blueLed, 1);
+        break;
+    case WHITE:
+        output(redLed, 1);
+        output(greenLed, 1);
+        output(blueLed, 1);
+        break;
+    case YELLOW:
+        output(redLed, 1);
+        output(greenLed, 1);
+        output(blueLed, 0);
+        break;
+    case CYAN:
+        output(redLed, 0);
+        output(greenLed, 1);
+        output(blueLed, 1);
+        break;
+    case PURPLE:
+        output(redLed, 1);
+        output(greenLed, 0);
+        output(blueLed, 1);
+        break;
+    case BLACK:
+    default:
+        output(redLed, 0);
+        output(greenLed, 0);
+        output(blueLed, 0);
+    }
 }
 
 /*------------Output Control------------*/
@@ -110,8 +176,8 @@ float fmap(float x, float in_min, float in_max, float out_min, float out_max)
 
 // This function reads from a 0-25V DC sensor and returns its voltage
 float voltRead(byte vSensor)
-{   
-    return fmap(analogRead(vSensor), 0, 1023, 0.0, 25.0);   // read from sensor and ajust the scale to 0-25V
+{   float f = fmap(analogRead(vSensor), 0, 1023, 0.0, 25.0);   // read from sensor and ajust the scale to 0-25V
+    return f;
 }
 
 // This function maintains the voltage in the supercapacitors between STARTCHARGINGVOLTAGE and STOPCHARGINGVOLTAGE
@@ -129,11 +195,32 @@ void voltControl()
     }
 }
 
+// This functions blocks the code execution until a certain voltage is reached inside the supercapacitors
+void waitForVoltage(int volts)
+{
+    float voltage = voltRead(voltSensor);
+    if (voltage < volts)
+    {
+        output(voltSSRelay, 1);
+        while (voltage < volts)
+        {
+            voltage = voltRead(voltSensor);
+        }
+        output(voltSSRelay, 0);
+    }
+}
+
 /*------------Voltage Control-----------*/
 
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
+    pinMode(redLed, OUTPUT);
+    pinMode(greenLed, OUTPUT);
+    pinMode(blueLed, OUTPUT);
+
+    setColor(YELLOW);
+
     pinMode(secBuoy, INPUT);
     pinMode(lowSurfaceBuoy, INPUT);
     pinMode(highSurfaceBuoy, INPUT);
@@ -155,22 +242,36 @@ void setup()
     pinMode(outFan, OUTPUT);
     pinMode(PSUFan, OUTPUT);
     pinMode(inFan, OUTPUT);
-    pinMode(redLed, OUTPUT);
-    pinMode(greenLed, OUTPUT);
-    pinMode(blueLed, OUTPUT);
+
+    output(voltSSRelay, 0);
+    output(voltRelay, 1);
+    output(ACInverter, 0);
+    output(wellPump, 0);
+    output(UVPump, 0);
+    output(endPump, 0);
+    output(UVRelay, 0);
+    output(filterRelay, 0);
+    output(outFan, 1);
+    output(PSUFan, 1);
+    output(inFan,  1);
+
     #if GUI
         pinMode(screenSensor, INPUT);
         pinMode(screenRelay, OUTPUT);
+        output(screenRelay, 0);
     #endif
     #if GUI
-        Serial1.begin(115200);
+       Serial1.begin(115200);
     #endif
-        
+
+    waitForVoltage(STARTCHARGINGVOLTAGE - 1);
+    output(voltRelay, 0);
+    setColor(CYAN);
 }
 
 // Add the main program code into the continuous loop() function
 void loop()
 {
     voltControl();
-
+    
 }
