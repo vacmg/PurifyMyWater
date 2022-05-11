4614e7ab54a97f12acaa8237919372210390c32b
Vasapg <victoralejandro.sanz@alumnos.upm.es>
2022-05-11 22:08:08 +0200

    Still working on Temperature, interface already done

--- H2O_GUI/H2O_UI/H2O_UI.ino
+++ H2O_GUI/H2O_UI/H2O_UI.ino
@@ -1073,13 +1073,13 @@ void drawTemperature() {
     {
         case 1:
             setFontSizeArray(fontSizes, 1, 1, 1, 2, 2, 2);
-            draw6ButtonsLayout("Temp. Refresh Rate ", F("System Stop Temp."), F("PSU Fan Start Temp."), String(((double)TEMPCHECKTIME)/1000) + "s", String(STOPWORKINGTEMP)+"C", "40C",
+            draw6ButtonsLayout(F("Temp. Refresh Rate "), F("System Stop Temp."), F("PSU Fan Start Temp."), String(((double)TEMPCHECKTIME)/1000) + "s", String((double)STOPWORKINGTEMP) + "C", String((double)STARTPSUTEMP) + "C",
                                true, true, true, fontSizes);
             break;
         case 2:
             setFontSizeArray(fontSizes, 1, 1, 1, 2, 2, 2);
-            draw6ButtonsLayout(F("PSU Fan Stop Temp."), F("Case Fan Start Temp."), F("Case Fan Stop Temp."), "35C", "38C",
-                               "34C", true, true, true, fontSizes);
+            draw6ButtonsLayout(F("PSU Fan Stop Temp."), F("Case Fan Start Temp."), F("Case Fan Stop Temp."), String((double)STOPPSUTEMP)+"C", String((double)STARTCASETEMP)+"C",
+                               String((double)STOPCASETEMP)+"C", true, true, true, fontSizes);
             break;
     }
 }
@@ -1276,7 +1276,7 @@ void clickTemperature()
     {
         switch (page)
         {
-            case 1: // Temp. Refresh Rate
+            case 1:{ // Temp. Refresh Rate
                 double tempVal = getNumInput("Temp. Refresh Rate", "s", TEMPCHECKTIME);
                 if (!isnan(tempVal)) // if getNumInput was not cancelled
                 {
@@ -1284,13 +1284,28 @@ void clickTemperature()
                     {
                         debug(String(F("TEMPCHECKTIME UPDATED: ")) + TEMPCHECKTIME + String(F(" --> ")) +
                               tempVal);
-                        TEMPCHECKTIME = (long) tempVal * 1000;
+                        TEMPCHECKTIME = tempVal * 1000;
                         // TODO send new setting
                     }
                 }
                 changeStatus(LOADPAGETEMPERATURE); // reload page with new config value
                 drawBackground(); // to print again the page after calling getNumInput, we need to draw the background too
-                break;
+                break;}
+            case 2:{ //PSU Fan Stop Temp.
+                double tempVal = getNumInput("PSU Fan Stop Temp.", "C", STOPPSUTEMP);
+                if (!isnan(tempVal)) // if getNumInput was not cancelled
+                {
+                    if (tempVal > 0 && tempVal < STARTPSUTEMP)
+                    {
+                        debug(String(F("STOPPSUTEMP UPDATED: ")) + STOPPSUTEMP + String(F(" --> ")) +
+                              tempVal);
+                        STOPPSUTEMP = (double)tempVal;
+                        // TODO send new setting
+                    }
+                }
+                changeStatus(LOADPAGETEMPERATURE); // reload page with new config value
+                drawBackground(); // to print again the page after calling getNumInput, we need to draw the background too
+                break;}
         }
     }
 
@@ -1298,21 +1313,37 @@ void clickTemperature()
     {
         switch (page)
         {
-            case 1: // System Stop Temp.
+            case 1:{ // System Stop Temp.
                 double tempVal = getNumInput("Temp. Refresh Rate", "C", STOPWORKINGTEMP);
                 if (!isnan(tempVal)) // if getNumInput was not cancelled
                 {
-                    if (tempVal > 0)
+                    if (tempVal > 0 && tempVal>STARTPSUTEMP && tempVal>STARTCASETEMP)
                     {
                         debug(String(F("STOPWORKINGTEMP UPDATED: ")) + STOPWORKINGTEMP + String(F(" --> ")) +
                               tempVal);
-                        STOPWORKINGTEMP = tempVal;
+                        STOPWORKINGTEMP = (double)tempVal;
                         // TODO send new setting
                     }
                 }
                 changeStatus(LOADPAGETEMPERATURE); // reload page with new config value
                 drawBackground(); // to print again the page after calling getNumInput, we need to draw the background too
-                break;
+                break;}
+
+            case 2:{ // Case Fan Start Temp.
+                double tempVal = getNumInput("Case Fan Start Temp.", "C", STARTCASETEMP);
+                if (!isnan(tempVal)) // if getNumInput was not cancelled
+                {
+                    if (tempVal > 0  && tempVal > STOPCASETEMP  && tempVal < STOPWORKINGTEMP)
+                    {
+                        debug(String(F("STARTCASETEMP UPDATED: ")) + STARTCASETEMP + String(F(" --> ")) +
+                              tempVal);
+                        STARTCASETEMP = (double)tempVal;
+                        // TODO send new setting
+                    }
+                }
+                changeStatus(LOADPAGETEMPERATURE); // reload page with new config value
+                drawBackground(); // to print again the page after calling getNumInput, we need to draw the background too
+                break;}
         }
     }
 
@@ -1320,21 +1351,37 @@ void clickTemperature()
     {
         switch (page)
         {
-            case 1: // PSU Fan Start Temp.
-                double tempVal = getNumInput("PSU Fan Start Temp.", "s", STARTPSUTEMP);
+            case 1:{ // PSU Fan Start Temp.
+                double tempVal = getNumInput("PSU Fan Start Temp.", "C", STARTPSUTEMP);
                 if (!isnan(tempVal)) // if getNumInput was not cancelled
                 {
-                    if (tempVal > 0)
+                    if (tempVal > 0  && tempVal > STOPPSUTEMP  && tempVal < STOPWORKINGTEMP)
                     {
                         debug(String(F("STARTPSUTEMP UPDATED: ")) + STARTPSUTEMP + String(F(" --> ")) +
                               tempVal);
-                        STARTPSUTEMP = tempVal;
+                        STARTPSUTEMP = (double)tempVal;
                         // TODO send new setting
                     }
                 }
                 changeStatus(LOADPAGETEMPERATURE); // reload page with new config value
                 drawBackground(); // to print again the page after calling getNumInput, we need to draw the background too
-                break;
+                break;}
+
+            case 2:{ //Case Fan Stop Temp.
+                double tempVal = getNumInput("Case Fan Stop Temp.", "C", STOPCASETEMP);
+                if (!isnan(tempVal)) // if getNumInput was not cancelled
+                {
+                    if (tempVal > 0 && tempVal < STARTCASETEMP)
+                    {
+                        debug(String(F("STOPPSUTEMP UPDATED: ")) + STOPCASETEMP + String(F(" --> ")) +
+                              tempVal);
+                        STOPCASETEMP = (double)tempVal;
+                        // TODO send new setting
+                    }
+                }
+                changeStatus(LOADPAGETEMPERATURE); // reload page with new config value
+                drawBackground(); // to print again the page after calling getNumInput, we need to draw the background too
+                break;}
         }
     }
 
