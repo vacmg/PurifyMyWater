092539f69a76705fe947e560d074c5c1d1740954
vacmg <70143287+vacmg@users.noreply.github.com>
2023-12-29 14:12:31 +0100

    Fix SettingsParser Headers & M1.TD2

--- Source/PurifyMyWater/components/SettingsStorage/include/SettingsParser.h
+++ Source/PurifyMyWater/components/SettingsStorage/include/SettingsParser.h
@@ -8,7 +8,7 @@ class SettingsParser
     public:
         // Enum that stores the possible errors returned by the API Settings
         typedef enum{NO_ERROR = 0, INVALID_MAP_ERROR, INVALID_PATH_ERROR,
-                     IO_ERROR} ParserError_t;
+                     FILESYSTEM_ERROR} ParserError_t;
 
         // returns the errno attribute which corresponds to the exit status of the last 
         // call to any of the class methods
@@ -19,16 +19,16 @@ class SettingsParser
 
         // This function parses the map to a JSON string using cJSON.h library, then calculate 
         // the checksum value of it and write the json string to {path}.json and the checksum to {path}.chk
-        static bool writeToFile(Settings::SettingsMap_t* map, char* path);
+        static bool writeToFile(Settings::SettingsMap_t* map, const char* path);
 
         // This function reads the files {path}.json and {path}.chk to retrieve the json settings.
-        static Settings::SettingsMap_t* readFromFile(char* path);
+        static Settings::SettingsMap_t* readFromFile(const char* path);
 
     private:
         static ParserError_t Errno;
 
         // performs a checksum calculation using CRC32 algorithm and returns the hashed value
-        static int checksum(char* data, size_t size);
+        static int checksum(const char* data, size_t size);
         
 };
 
